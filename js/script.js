754122db42a88233baf53323140f4cfdd9de6d14
Nathan Le <nha.le@tekos.net>
2023-04-03 23:14:16 -0400

    Feat: testimonials in home page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,47 @@ const generateCurrentYearForFooter = () => {
 };
 
 generateCurrentYearForFooter();
+
+/* SLIDER */
+const reviewerPhotoElements = document.querySelectorAll('.reviewer-photo');
+const reviewContentElements = document.querySelectorAll('.review-content');
+const ratingsElements = document.querySelectorAll('.ratings');
+const reviewerNameElements = document.querySelectorAll('.reviewer-name');
+const reviewerTitleElements = document.querySelectorAll('.reviewer-title');
+const sliderIndicators = document.querySelectorAll('.slider-indicator');
+console.log('📢[script.js:16]: sliderIndicators: ', sliderIndicators);
+
+const handleShowSelectedReview = (nodesArray, selectedIndicator) => {
+	const indexOfCurrentSelected = selectedIndicator - 1;
+	nodesArray.forEach((ele, index) => {
+		if (index !== indexOfCurrentSelected) ele.classList.add('hide');
+		else ele.classList.remove('hide');
+	});
+};
+
+sliderIndicators.forEach((ele) => {
+	ele.addEventListener('click', (event) => {
+		const indicatorNumber = ele.getAttribute('name');
+		const classNames = ele.className;
+		if (classNames.includes('selected')) return;
+
+		// Change photo
+		handleShowSelectedReview(reviewerPhotoElements, indicatorNumber);
+		// Change review content
+		handleShowSelectedReview(reviewContentElements, indicatorNumber);
+		// Change reviewer name
+		handleShowSelectedReview(reviewerNameElements, indicatorNumber);
+		// Change reviewer title
+		handleShowSelectedReview(reviewerTitleElements, indicatorNumber);
+		// Change ratings block
+		handleShowSelectedReview(ratingsElements, indicatorNumber);
+		// Change selected Indicator
+		sliderIndicators.forEach((indicator) => {
+			if (indicatorNumber === indicator.getAttribute('name')) {
+				indicator.classList.add('selected');
+			} else {
+				indicator.classList.remove('selected');
+			}
+		});
+	});
+});
